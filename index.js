7b94a13c08a89229b80a24f3cffa62300d3f537b
kartikey <kartikkey0414@gmail.com>
2025-03-17 10:31:39 +0530

    JSON response formatted

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const Amadeus = require("amadeus");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 
 // Initialize Amadeus API
